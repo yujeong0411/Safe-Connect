c48443a1a2ca53c24b17bed1e7d0110a8ca39cd4
김민철 <kmc4889@naver.com>
2025-01-20 13:56:33 +0900

    feat/backend init setting 김민철 20250120

---
+++ backend/src/main/java/c207/camference/CamferenceApplication.java
@@ -0,0 +1,13 @@
+package c207.camference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamferenceApplication.class, args);
+    }
+
+}
