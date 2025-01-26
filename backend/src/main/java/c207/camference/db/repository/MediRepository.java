728ef3e23b38cbbc12a572996c73bd6565a9a2a8
박찬미 <pcm0902@naver.com>
2025-01-26 16:20:15 +0900

    박찬미 S12P11C207-146 User 약물, 기저질환 전체조회 구현 환료 20250126

--- backend/src/main/java/c207/camference/db/repository/MediRepository.java
+++ backend/src/main/java/c207/camference/db/repository/MediRepository.java
@@ -3,5 +3,9 @@ package c207.camference.db.repository;
 import c207.camference.db.entity.Medi;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MediRepository extends JpaRepository<Medi, Integer> {
+    // mediIsActice = true 인 약물/질환만 조회
+    List<Medi> findByMediCategory_MediCategoryIdAndMediIsActiveTrue(Integer mediCategoryId);
 }
