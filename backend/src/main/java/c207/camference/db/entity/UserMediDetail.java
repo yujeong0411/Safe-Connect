ae9af729b85e446be76db1eb3f32aa40995b32cd
박찬미 <pcm0902@naver.com>
2025-01-30 18:56:13 +0900

    박찬미 S12P11C207-163 회원의 약물, 질환 수정 구현 완료 20250130

--- backend/src/main/java/c207/camference/db/entity/UserMediDetail.java
+++ backend/src/main/java/c207/camference/db/entity/UserMediDetail.java
@@ -51,12 +51,40 @@ public class UserMediDetail {
         this.user = user;
     }
 
-    // 의약질환 매핑 정보 업데이트
-    public void updateMediMappings(List<Medi> medis) {
-        // 기존 매핑 제거
-        userMediMappings.clear();
+    // 의약질환 매핑 생성
+    public void createMediMappings(List<Medi> medis) {
+        for (Medi medi : medis) {
+            boolean exists = userMediMappings.stream()
+                    .anyMatch(mapping -> mapping.getMedi().equals(medi));
+            // 중복 방지
+            if (!exists) {
+                userMediMappings.add(new UserMediMapping(this, medi));
+            }
+        }
+    }
 
-        // 새로운 매핑 추가
-        medis.forEach(medi -> userMediMappings.add(new UserMediMapping(this, medi)));
+    // 의약짏환 매핑 수정. 파라미터는 request로 들어온 medi 객체 리스트
+    public void updateMediMappings(List<Medi> medis) {
+        for (UserMediMapping mapping : userMediMappings) {
+            // 기존에 있던 의약질환이 request에 포함되어 있지 않다면(=회원이 선택하지 않았다) 비활성화
+            if (!medis.contains(mapping.getMedi())) {
+                mapping.deactivate();
+            }
+        }
+
+        for (Medi medi : medis) {
+            // 기존에 있던 medi는 true 반환
+            boolean exists = userMediMappings.stream()
+                    .anyMatch(mapping -> mapping.getMedi().equals(medi));
+            // 기존에 없던 medi는 추가해줌
+            if (!exists) {
+                userMediMappings.add(new UserMediMapping(this, medi));
+            } else {
+                // 기존에 있었지만 비활성화된 medi는 다시 활성화
+                userMediMappings.stream()
+                        .filter(mapping -> mapping.getMedi().equals(medi))
+                        .forEach(mapping -> mapping.activate());
+            }
+        }
     }
 }
--- backend/src/main/java/c207/camference/db/entity/UserMediMapping.java
+++ backend/src/main/java/c207/camference/db/entity/UserMediMapping.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Index;
 
 @Entity
 @Getter
@@ -16,16 +17,28 @@ public class UserMediMapping {
     @Column(name = "user_medi_mapping_id")
     private Integer id;
 
+    @Index(name = "idx_user_medi", columnNames = "user_id, medi_id")
     @ManyToOne(fetch = FetchType.LAZY) // 지연로딩. user_medi_mapping 엔티티 조회 시 user_medi_detail 엔티티를 즉시 가져오지 않음.
-    @JoinColumn(name = "user_medi_detail_id", nullable = false) // FK
+    @JoinColumn(name = "user_id", nullable = false) // FK
     private UserMediDetail userMediDetail; // 관계의 주인
 
     @ManyToOne(fetch = FetchType.LAZY) // 지연로딩. user_medi_mapping 엔티티 조회 시 medi 엔티티를 즉시 가져오지 않음.
     @JoinColumn(name = "medi_id", nullable = false)
     private Medi medi;
 
+    @Column(nullable = false)
+    private Boolean mediIsActive = true;
+
     public UserMediMapping(UserMediDetail userMediDetail, Medi medi) {
         this.userMediDetail = userMediDetail;
         this.medi = medi;
+    }
+
+    public void activate() {
+        this.mediIsActive = true;
+    }
+
+    public void deactivate() {
+        this.mediIsActive = false;
     }
 }
