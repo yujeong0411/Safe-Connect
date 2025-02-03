eb97129a71573ce4260d782c9ab4ad7c92f8569f
김민철 <kmc4889@naver.com>
2025-02-03 10:21:58 +0900

    김민철 S12P11C207-202 병원 이송내역 조회 진행 중 180 완성 후 rebase해서 작성해야함 20250203

--- backend/src/main/java/c207/camference/db/repository/patient/PatientRepository.java
+++ backend/src/main/java/c207/camference/db/repository/patient/PatientRepository.java
@@ -1,9 +1,11 @@
 package c207.camference.db.repository.patient;
 
 import c207.camference.db.entity.patient.Patient;
+import c207.camference.db.entity.report.Transfer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient,Integer> {
+    Patient findByTransfer(Transfer transfer);
 }
