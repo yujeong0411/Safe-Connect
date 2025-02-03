1385c4269c04bf238e0d28bb8cee6254936ef3f4
박찬미 <pcm0902@naver.com>
2025-02-04 01:38:50 +0900

    박찬미 S12P11C207-206 환자 이송 수락 거절 기능 구현 완료 20250204

--- backend/src/main/java/c207/camference/api/response/hospital/AcceptTransferResponse.java
+++ backend/src/main/java/c207/camference/api/response/hospital/AcceptTransferResponse.java
@@ -1,9 +1,11 @@
 package c207.camference.api.response.hospital;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Getter
 @AllArgsConstructor
 public class AcceptTransferResponse {
 
