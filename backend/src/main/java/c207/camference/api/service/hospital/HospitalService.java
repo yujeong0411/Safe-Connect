6a9a715591d192d1319ec27799578566df03ede5
박찬미 <pcm0902@naver.com>
2025-02-07 15:40:03 +0900

    박찬미 S12P11C207-253 병원 transfer/status requestBody 수정 및 medi controller token 수정 완료 20250207

--- backend/src/main/java/c207/camference/api/service/hospital/HospitalService.java
+++ backend/src/main/java/c207/camference/api/service/hospital/HospitalService.java
@@ -1,12 +1,13 @@
 package c207.camference.api.service.hospital;
 
 
+import c207.camference.api.request.hospital.TransferStatusRequest;
 import org.springframework.http.ResponseEntity;
 
 public interface HospitalService {
     ResponseEntity<?> getTransferAccepted();
     ResponseEntity<?> getTransferAcceptedDetail(int transferId);
-    ResponseEntity<?> respondToTransfer(int patientId, String status);
+    ResponseEntity<?> respondToTransfer(TransferStatusRequest request);
     ResponseEntity<?> transferRequest();
     ResponseEntity<?> transferRequestDetail(int dispatchId);
 }
