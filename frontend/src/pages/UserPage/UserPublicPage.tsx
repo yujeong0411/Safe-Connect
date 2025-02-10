146c37aa0022db73e476228b5e5cea7981db4c4c
최유정 <choiyujeong0411@gmail.com>
2025-02-10 18:00:11 +0900

    최유정 S12P11C207-271 로그아웃 수정중

--- frontend/src/pages/UserPage/UserPublicPage.tsx
+++ frontend/src/pages/UserPage/UserPublicPage.tsx
@@ -5,7 +5,7 @@ const UserPublicPage = () => {
   return (
     <UserPublicTemplate>
       {/* 캐러셀을 가운데 정렬하고 크기 유지 */}
-      <div className="w-full max-w-[1400px] min-h-[600px] flex justify-center items-center">
+      <div className="w-full max-w-[1100px] max-h-[600px] flex justify-center items-center">
         <Carousel />
       </div>
     </UserPublicTemplate>
