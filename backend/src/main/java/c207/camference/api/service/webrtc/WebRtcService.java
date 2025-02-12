e258d5e5f3ba77a33f2c02f7aa36d7cb92471c2b
김성준 <sjkim2243@gmail.com>
2025-02-12 23:19:18 +0900

    김성준 S12P11C207-308 pre-KTAS 수정 완료  20250212

--- backend/src/main/java/c207/camference/api/service/webrtc/WebRtcService.java
+++ backend/src/main/java/c207/camference/api/service/webrtc/WebRtcService.java
@@ -1,5 +1,6 @@
 package c207.camference.api.service.webrtc;
 
+import c207.camference.api.request.dispatchstaff.PreKtasRequest;
 import io.openvidu.java.client.OpenViduHttpException;
 import io.openvidu.java.client.OpenViduJavaClientException;
 import org.springframework.http.ResponseEntity;
@@ -10,9 +11,10 @@ import java.io.IOException;
 public interface WebRtcService {
     // ResponseEntity<?> sendUrlMsg(String callerPhone) throws OpenViduJavaClientException, OpenViduHttpException;
     String speechToText(MultipartFile audioFile) throws IOException;
-    String textSummary(String speechToText, String mode);
+    String textSummary(String speechToText);
     ResponseEntity<?> saveSummary(Integer callId, String text, String summary);
 
     String makeSession(String customSessionId) throws OpenViduJavaClientException, OpenViduHttpException;
     String makeUrl(String sessionId);
+    Integer getPreKtas(PreKtasRequest request);
 }
