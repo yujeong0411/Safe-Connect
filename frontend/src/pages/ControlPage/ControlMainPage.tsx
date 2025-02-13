3638f95328fed84b9d7d0d6e224e8b8bff2d27b4
김민철 <kmc4889@naver.com>
2025-02-13 15:37:49 +0900

    김민철 caller 위치 보내기 및 그 위치 완료 20250213

--- frontend/src/pages/ControlPage/ControlMainPage.tsx
+++ frontend/src/pages/ControlPage/ControlMainPage.tsx
@@ -1,31 +1,8 @@
 // ControlMainPage.tsx
 import ControlMainTemplate from '@features/control/components/ControlMainTemplate.tsx';
 import CallRecordForm from '@features/control/components/CallRecordForm.tsx';
-import { useControlsseStore } from '@/store/control/controlsseStore.ts';
-import { useEffect } from 'react';
-import { useControlAuthStore } from '@/store/control/controlAuthStore.tsx';
 
 const ControlMainPage = () => {
-  const { connect } = useControlsseStore();
-  const { isAuthenticated } = useControlAuthStore(); // 인증 상태 확인
-
-  useEffect(() => {
-    const connectSSE = () => {
-      const userName = localStorage.getItem("userName");
-      if (userName && isAuthenticated) {
-        connect(userName);
-      }
-    };
-
-    connectSSE(); // 초기 연결
-
-    // 새로고침 시 연결
-    window.addEventListener('load', connectSSE);
-
-    return () => {
-      window.removeEventListener('load', connectSSE);
-    };
-  }, [connect, isAuthenticated]);
 
   return (
     <ControlMainTemplate>
